f5ea30dd2f5d1de8d452ea51b2b1e571efe98a6d
Allen <Hagotchaaa@gmail.com>
2026-03-29 22:56:58 -0700

    added profile-new page

---
+++ config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = "https://rlmcwbaiuowbkgfzdlme.supabase.co";
+window.SUPABASE_KEY = "sb_publishable_5yQzcEdu9lVzvMHy_vyW9g_Pg1TWUEU";
--- www/config.js
+++
@@ -1,2 +0,0 @@
-window.SUPABASE_URL = "https://rlmcwbaiuowbkgfzdlme.supabase.co";
-window.SUPABASE_KEY = "sb_secret_C2UuIVhb3-a4dbbAmi4TvQ_z_UU3hwv";
